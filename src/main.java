68dd69df061f3b2924f9a722dd178359291cfadc
saulg7746 <saulg7746@gmail.com>
2021-09-24 22:57:17 -0700

    testing

--- src/main.java
+++ src/main.java
@@ -9,7 +9,7 @@ public class main {
 
 	
 	public static void main(String args[]) {
-		System.out.println("Hello WOrld!");
+		System.out.println("Hello World!");
 		
 //		MonsterSearchCriteria mon = new MonsterSearchCriteria("Stratos");
 //		mon.nameExact("Elemental HERO Stratos");
